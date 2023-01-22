060a897513ea2c592db312f5aa94d273eb17d4b2
HikaruSato <110364092+Omochi800@users.noreply.github.com>
2023-01-22 06:32:01 +0000

    01221531

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,31 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  if@user.update(user_params)
+    redirect_to admin_user_path
+  else
+    render:edit
+  end
+  end
+
+  private
+
+  def
+    customer_params
+    params.require(:user).permit(:name,:user_name,:address,:email,:is_deleted)
+  end
 end
