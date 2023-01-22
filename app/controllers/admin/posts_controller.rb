4c66aae8e31fd88b9c789c1c099d2eab6989f8a0
HikaruSato <110364092+Omochi800@users.noreply.github.com>
2023-01-22 12:21:48 +0000

    202301222121

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,19 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:text,:user_id,:image)
+  end
 end
